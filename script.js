96c3142ccf40c3894d73e7353d23674591cbd8af
Bhagavati Braun <bhagavati.braun@gmail.com>
2020-08-30 18:50:46 +0300

    Struggling with Delete button
    Exploratory work in figuring out how to delete each library book. More
    work to be done!

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const library = document.querySelector('#library');
 const body = document.querySelector('#body');
 const bookForm = document.querySelector('#newBook');
 
+// An array to store each card
+let myLibrary = [];
+let card, newTitle, newAuthor, newPages, newRead, delBook;
+
 
 // Add book button
 const addBookBtn = document.createElement('button');
@@ -16,9 +20,6 @@ bookForm.classList.toggle('formHidden');
 addBookBtn.addEventListener('click', () => bookForm.classList.toggle('formOpen'));
 // Hiding form after submission is in addBooktoLibrary() function
 
-
-// An array to store each card
-let myLibrary = [];
 
 
 
@@ -40,15 +41,15 @@ function Book(title, author, pages, read, index){
 	this.dataIndex = index;
 }
 
-Book.prototype.toggleRead = function() {
-	let index = this.dataIndex;
-	if (myLibrary[index].read) {
-		myLibrary[index].read = 0;
-	}else {
-		myLibrary[index].read = 1;
-	};
-	render();
-}
+// Book.prototype.toggleRead = function() {
+// 	let index = this.dataIndex;
+// 	if (myLibrary[index].read) {
+// 		myLibrary[index].read = 0;
+// 	}else {
+// 		myLibrary[index].read = 1;
+// 	};
+// 	render();
+// }
 
 Book.prototype.about = function() {
 	return (`${title} by ${author}, ${pages} pages, ${read}`);
@@ -58,7 +59,7 @@ Book.prototype.about = function() {
 
 
 
-// Create a book from the constructor, add it to myLibrary array
+// Create a book from the form, add it to myLibrary array
 function addBooktoLibrary() {
 	
 	// Collect data from form
@@ -75,7 +76,6 @@ function addBooktoLibrary() {
 	
 	// Add book to myLibrary
 	myLibrary.push(book); 
-	console.log(myLibrary[myLibrary.length - 1]);
 	
 	// Hide form
 	bookForm.classList.remove('formOpen');
@@ -87,7 +87,7 @@ function addBooktoLibrary() {
 	document.querySelector('#fread').value = '';
 	render();
 }
-let card, newTitle, newAuthor, newPages, newRead, delBook;
+
 
 // Display books on screen
 function render() {
@@ -97,15 +97,18 @@ function render() {
 	}
 	
 	// Display each book in myLibrary
-	for (book of myLibrary) {
-			console.log(book.title);
-			console.log(book.dataIndex);
+	//for (book of myLibrary) {
+	myLibrary.forEach(function (book) {
+			console.log(book.title + '...' + book.dataIndex);
 		card = document.createElement('div');
 		newTitle = document.createElement('h1');
 		newAuthor = document.createElement('p');
 		newPages = document.createElement('p');
 		newRead = document.createElement('button');
 		delBook = document.createElement('button');
+		delBook.addEventListener('click', deleteBook);
+		//(e) => {myLibrary.splice(myLibrary.indexOf(book), 1);card.remove();
+		//});		
 		library.appendChild(card);
 		card.appendChild(newTitle);
 		card.appendChild(newAuthor);
@@ -123,30 +126,23 @@ function render() {
 			newRead.textContent = 'Not Read';
 		}
 		delBook.textContent = 'Delete';
-	}
-	
-	
-	for (book of myLibrary) {
 		
-	delBook.addEventListener('click', function () {
-		let index = book.dataIndex;
-		if (confirm(`Do you want to delete ${index} from your library?`)) {
-		}else {
-			return;
-		}
-		myLibrary.splice(index, 1);
-		render();
-		return;
 	});
-	newRead.addEventListener('click', () => {
-		book.toggleRead();
-		//(book.read ? book.read = 0 : book.read = 1);
-		//render();
-	});
-}
+	
+	
 }
 
-//delBook.addEventListener('click', console.log('clicked'));
+
+function deleteBook(book, index) {
+	//document.getElementById('test').textContent += `book is ${book} .... index is ${index}`;
+	if (confirm(`Do you want to delete ${myLibrary[index]} from your library?`)) {
+		myLibrary.splice(index, 1);
+		card.remove();
+		console.log(myLibrary);
+		render();
+	}
+}
+// delBook.addEventListener('click', console.log('clicked'));
 
 function randomColor() {
 	let r = Math.floor(Math.random() * 256);
@@ -158,16 +154,53 @@ function randomColor() {
 // Stored books auto-populate
 render();
 
-/*/========TO DO=========\*\
 
-Delete and read buttons.
+
+
+//========NOTES========\\
+/*
+	deleteBook
+		Currently
+			code is deep in the render() function.
+			The problem persists that the item deleted is the LAST item in the array.
+			*card.remove is GREAT => don't have to remove and re-render all the cards!
+			still unsure how to get the index of the current delete button.
+				Maybe there's something in creating an iterator and adding an ID to each delete button as they're created?
+				This would have to be the same for read...?
+			need to get the display to confirm deletion up and runing too.
+			
+			WEIRDSHIT:
+				card.remove removes LAST card
+						(only seems to remove the one, another card won't be removed)
+				while
+				splice removes FIRST book.
+			  Currently we're left with a ever shrinking array (item[0] gets removed)
+			  	while item[-1] seems to be removed on the first iteration and NONE thereafter.
+				This junk is messed up!
+			  RENDER
+			  	gives a more true view of what is being deleted
+			  ALSO
+				Objects keep their index even when another object is removed.
+			
+	toggle read
+		in limbo
+		
 	
-Display
-	make everything look good
 
-Back to TOP for other functionality.
-/*/
 
-/*/===========THINGS TO STUDY============\*\
-	querySelector vs getElementById
-	*/
+//*/
+//button.addEventListener('click', listener, useCapture)
+
+// function buttonClick() {
+// 	if (button.getElementById == 'delBook') {
+// 		deleteBook();
+// 		return;
+		
+// 	}else if (button.getElementById == 'newRead') {
+// 		toggleRead();
+// 		return;
+// 	}else if (button.getElementById == 'addBookBtn') {
+// 		addBooktoLibrary();
+// 		return
+// 	}
+// }
