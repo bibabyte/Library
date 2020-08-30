03686d9726615147ff3e609d90c53232888fe1a6
Bhagavati Braun <bhagavati.braun@gmail.com>
2020-08-30 15:11:40 +0300

    Button functionality and object indexing

--- script.js
+++ script.js
@@ -20,22 +20,34 @@ addBookBtn.addEventListener('click', () => bookForm.classList.toggle('formOpen')
 // An array to store each card
 let myLibrary = [];
 
-//=========TESTING=========\\
-let prince = new Book('The Little Prince', 'Antoine de Saint-Exupery', 37, 0);
 
-let gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 197, 1);
+
+//=========TESTING=========\\
+let prince = new Book('The Little Prince', 'Antoine de Saint-Exupery', 37, 0, 0);
+
+let gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 197, 1, 1);
 myLibrary.push(prince, gatsby)
+//=========================\\
+
+
 
 // Book constructor and prototype funcitons
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, index){
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
+	this.dataIndex = index;
 }
 
-Book.prototype.sayTitle = function() {
-	console.log(title);
+Book.prototype.toggleRead = function() {
+	let index = this.dataIndex;
+	if (myLibrary[index].read) {
+		myLibrary[index].read = 0;
+	}else {
+		myLibrary[index].read = 1;
+	};
+	render();
 }
 
 Book.prototype.about = function() {
@@ -55,8 +67,11 @@ function addBooktoLibrary() {
 	let pages = document.querySelector('#fpages').value;
 	let read = document.querySelector('#fread').checked;
 	
+	// Find index number through myLibrary array
+	let index = myLibrary.length;
+	
 	// Create Book object
-	let book = new Book(title, author, pages, read);
+	let book = new Book(title, author, pages, read, index);
 	
 	// Add book to myLibrary
 	myLibrary.push(book); 
@@ -72,7 +87,7 @@ function addBooktoLibrary() {
 	document.querySelector('#fread').value = '';
 	render();
 }
-
+let card, newTitle, newAuthor, newPages, newRead, delBook;
 
 // Display books on screen
 function render() {
@@ -83,23 +98,23 @@ function render() {
 	
 	// Display each book in myLibrary
 	for (book of myLibrary) {
-		console.log(book.title);
-		
-		let newDiv = document.createElement('div');
-		let newTitle = document.createElement('h1');
-		let newAuthor = document.createElement('p');
-		let newPages = document.createElement('p');
-		let newRead = document.createElement('button');
-		let delBook = document.createElement('button');
-		library.appendChild(newDiv);
-		newDiv.appendChild(newTitle);
-		newDiv.appendChild(newAuthor);
-		newDiv.appendChild(newPages);
-		newDiv.appendChild(newRead);
-		newDiv.appendChild(delBook);
-		newDiv.style.backgroundColor = randomColor();
-		newDiv.className = 'book';
-		newTitle.textContent = `${book.title}`;
+			console.log(book.title);
+			console.log(book.dataIndex);
+		card = document.createElement('div');
+		newTitle = document.createElement('h1');
+		newAuthor = document.createElement('p');
+		newPages = document.createElement('p');
+		newRead = document.createElement('button');
+		delBook = document.createElement('button');
+		library.appendChild(card);
+		card.appendChild(newTitle);
+		card.appendChild(newAuthor);
+		card.appendChild(newPages);
+		card.appendChild(newRead);
+		card.appendChild(delBook);
+		card.style.backgroundColor = randomColor();
+		card.className = 'book';
+		newTitle.textContent = book.title;
 		newAuthor.textContent = `by ${book.author}`;
 		newPages.textContent = `${book.pages} pages`;
 		if (book.read) {
@@ -109,12 +124,29 @@ function render() {
 		}
 		delBook.textContent = 'Delete';
 	}
+	
+	
+	for (book of myLibrary) {
+		
+	delBook.addEventListener('click', function () {
+		let index = book.dataIndex;
+		if (confirm(`Do you want to delete ${index} from your library?`)) {
+		}else {
+			return;
+		}
+		myLibrary.splice(index, 1);
+		render();
+		return;
+	});
+	newRead.addEventListener('click', () => {
+		book.toggleRead();
+		//(book.read ? book.read = 0 : book.read = 1);
+		//render();
+	});
+}
 }
 
-
-render();
-
-
+//delBook.addEventListener('click', console.log('clicked'));
 
 function randomColor() {
 	let r = Math.floor(Math.random() * 256);
@@ -123,12 +155,12 @@ function randomColor() {
 	return `rgb(${r},${g},${b})`;
 }
 
+// Stored books auto-populate
+render();
 
 /*/========TO DO=========\*\
 
-Render
-	Currently titles appear. 
-
+Delete and read buttons.
 	
 Display
 	make everything look good
